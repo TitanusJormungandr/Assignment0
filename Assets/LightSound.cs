98c921010b86d98ee58a6298eb247d115c0e1d80
Domenic Cannella <dac36@pct.edu>
2025-08-20 13:07:49 -0400

    Finished!

---
+++ Assets/LightSound.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class LightSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Stop();
+        }
+    }
+}
